5e7df696ef8cac3975e096d9e49972fb6c3fc3a8
Selective Couple <coupleselective@gmail.com>
2025-09-04 17:46:12 +0000

    next

--- src/app/e-safety-file/page.tsx
+++ src/app/e-safety-file/page.tsx
@@ -78,7 +78,7 @@ export default function ESafetyFilePage() {
                     </div>
                 </div>
                 <div className="relative h-80 lg:h-[400px] w-full">
-                    <Image src="https://placehold.co/800x600.png" fill sizes="100vw" style={{objectFit: 'contain'}} alt="e-safety file on a laptop" data-ai-hint="saas dashboard laptop" />
+                    <Image src="https://iili.io/JADbWWp.png" fill sizes="100vw" style={{objectFit: 'contain'}} alt="e-safety file on a laptop" data-ai-hint="saas dashboard laptop" />
                 </div>
             </div>
         </div>
