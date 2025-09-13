28388c182f839ff4225507ac29f8c0ccf9706c41
Selective Couple <coupleselective@gmail.com>
2025-09-13 14:49:15 +0000

    <a href="https://ibb.co/67SpdDsd"><img src="https://i.ibb.co/TxGnCPhC/im

--- src/app/e-safety-file/page.tsx
+++ src/app/e-safety-file/page.tsx
@@ -81,7 +81,7 @@ export default function ESafetyFilePage() {
                     </div>
                 </div>
                 <div className="relative h-80 lg:h-[400px] w-full">
-                    <Image src="https://iili.io/JADbWWp.png" fill sizes="100vw" style={{objectFit: 'contain'}} alt="e-safety file on a laptop" data-ai-hint="saas dashboard laptop" />
+                    <Image src="https://i.ibb.co/TxGnCPh/image-20.jpg" fill sizes="100vw" style={{objectFit: 'contain'}} alt="e-safety file on a laptop" data-ai-hint="saas dashboard laptop" />
                 </div>
             </div>
         </div>
