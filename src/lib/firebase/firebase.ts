977c2ba8e946b45bca6b48cd7363211e503efbd7
Ruan Koen <sitesafetyservices121@gmail.com>
2025-09-28 20:23:07 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBIWqnQLELz_wOemo-mc7HV6mYeuh6F

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBIWqnQLELz_wOemo-mc7HV6mYeuh6FQJM",
+  authDomain: "rak-site-safety-services-vn21z.firebaseapp.com",
+  projectId: "rak-site-safety-services-vn21z",
+  storageBucket: "rak-site-safety-services-vn21z.appspot.com",
+  messagingSenderId: "1048163634498",
+  appId: "1:1048163634498:web:f7c0ba3f89b17b6aa00cdb"
 };
 
 // Initialize Firebase
