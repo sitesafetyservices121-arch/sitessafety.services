2737c5ea31e888d95243592acd99dfa75a9c310d
Ruan Koen <sitesafetyservices121@gmail.com>
2025-09-29 15:54:35 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -20,13 +20,21 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'iili.io',
       }
     ],
     qualities: [75, 100],
   },
   // This is required to allow the Next.js dev server to be accessed from
   // the IDX preview window.
-  allowedDevOrigins: ['https://*.googleusercontent.com', 'https://*.cloudworkstations.dev'],
+  allowedDevOrigins: [
+    'https://*.googleusercontent.com',
+    'https://*.cloudworkstations.dev',
+    'https://6000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev'
+  ],
 
 };
 
