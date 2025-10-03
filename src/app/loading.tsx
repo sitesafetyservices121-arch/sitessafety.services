987ef54728fcb965aa3aa0f388f9643c57738f6d
Ruan Koen <sitesafetyservices121@gmail.com>
2025-10-03 22:05:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,5 @@
 import { TopLoader } from "@/components/top-loader";
 
 export default function Loading() {
-  return (
-    <div className="w-full h-full">
-      <TopLoader />
-    </div>
-  );
+  return <TopLoader />;
 }
