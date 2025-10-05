3e09ee3f63e089550437dbf146ab588f742302ed
Ruan Koen <sitesafetyservices121@gmail.com>
2025-10-05 21:53:56 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
-    "https://6000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev"
+    "https://6000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
+    "https://9000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev"
   ],
 };
 
