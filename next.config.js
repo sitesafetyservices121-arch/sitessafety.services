62995ee10c5f2a1dcc83a85c0e10fbd06ac46392
Ruan Koen <sitesafetyservices121@gmail.com>
2025-10-06 19:59:21 +0000

    local updates before pull

--- next.config.js
+++ next.config.js
@@ -2,9 +2,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
-    "https://6000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
-    "https://9000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev"
+    "https://6000-firebase-studio-1755150981545.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev"
   ],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' freeimage.host platform.linkedin.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: iili.io; font-src 'self';`
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ['iili.io'],
+  },
 };
 
 module.exports = nextConfig;
